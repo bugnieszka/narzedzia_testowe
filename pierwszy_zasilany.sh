59cb5c5bc46750b98aa451bb32b13529ddb9fd12
bugnieszka <shasirrlan@gmail.com>
2020-04-19 12:35:44 +0200

    Dodanie pliku pierwszy_zasilany.sh

---
+++ pierwszy_zasilany.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Ten program potrzebuje argumentow do wyswietlenia.
+
+echo "Witaj uzytkowniku $1"
+echo "Uruchomiles program: $0"
+echo "Podales argumenty $@"
+echo "Podales $# argumentow"
