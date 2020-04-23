32ef5473eb8924cd23f4fa87d93dd27637cb033b
bugnieszka <shasirrlan@gmail.com>
2020-04-23 20:08:28 +0200

    Zrobione zadanie3.sh

--- zadanie3.sh
+++ zadanie3.sh
@@ -4,14 +4,35 @@
 read -p "Podaj liczbę " n
 array=()
 suma=0
+max=0
+min="nima"
 for i in `seq $n`
 do
     temp=$RANDOM
     echo "${temp}"
-    array+=("${temp}")
+    array+=("${temp}") 
     let suma+=$temp
+
+    #if i=1 ; then
+    #    min="$temp"
+    #fi
+
+    if ((temp > max)) ; then
+        max="$temp"
+       fi
+
+       if [ $min == "nima" ] ; then
+           min=${array[0]}
+       fi
+          
+           if ((min > temp)) ; then
+                min="$temp"
+           fi
+       
 done
 
 echo "${array[@]}"
 echo "Suma tablicy to ${suma}"
 echo "Średnia tablicy to $((suma/n))"
+echo "Najmniejsza wartość tablicy to $min"
+echo "Największa wartość tablicy to $max"
